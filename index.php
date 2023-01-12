7dd2194376901c744f96bbad5fbb0f783921eb19
Adam Wise <wise.adam@student.greenriver.edu>
2023-01-12 17:21:47 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// this is my CONTROLLER for the hello project
+
+//Turn  on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+//Java equivelent  -> Base f3 = new Base()
+
+
+//Define a default route
+// what the user sees when the go to the homepage of the hello project
+$f3->route('GET /',function(){
+    echo'<h1>Pet Home<h1>';
+
+
+});
+
+
+
+
+//run Fat-Free
+$f3->run();
+// Java -> f3.run();
+
+
+
+
+
+
+
+
+
+
+
+?>
